8528d6cce37eeb7b5220cbf4e56532719b258e75
student <student@M30519>
2020-10-22 16:59:49 +0900

    commit2

--- SpringMVCProject/src/com/multicampus/controller/user/LoginController.java
+++ SpringMVCProject/src/com/multicampus/controller/user/LoginController.java
@@ -1,5 +1,7 @@
 package com.multicampus.controller.user;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,15 @@ public class LoginController {
 	public String login(UserVO vo, UserDAO userDAO) throws Exception {
 		if(userDAO.getUser(vo) != null) return "getBoardList.do";
 		else return "login.jsp";
+	}
+	
+	@RequestMapping(value="/loginout.do")
+	public String loginout(HttpSession session) throws Exception {
+		//로그아웃 요청한 브라우저와 매핑된 세션을 메모리에서 강제 종료
+		session.invalidate();
+		
+		//로그아웃 후에는 메인 페이지로 이동
+		return "index.jsp";
 	}
 
 }
